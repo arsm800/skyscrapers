109f9e3fb507f24d48c92e63c5262877ec1c8809
Andrew Smith-Mui <arsm800@gmail.com>
2016-06-27 14:17:46 -0400

    Fixed instance variable in skyscrapers show controller.  Removed each loop in skyscraper show view so each skyscraper's attributes are displayed.

--- app/controllers/skyscrapers_controller.rb
+++ app/controllers/skyscrapers_controller.rb
@@ -1,6 +1,12 @@
 class SkyscrapersController < ApplicationController
+
   def index
     @cities = City.find(params[:city_id])
     @skyscrapers = @cities.skyscrapers
+  end
+
+  def show
+    city = City.find(params[:city_id])
+    @skyscraper = city.skyscrapers.find(params[:id])
   end
 end
